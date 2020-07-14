9a5117f9c7cf61b9f0b5dcf07e882b16ac9fc532
harsh-s10 <51875985+harsh-s10@users.noreply.github.com>
2020-07-14 14:45:34 +0530

    Add files via upload

---
+++ User Authentication/user_aut/basic_admin/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from basic_admin.models import UserProfileInfo
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ User Authentication/user_aut/basic_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicAdminConfig(AppConfig):
+    name = 'basic_admin'
